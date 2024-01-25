69dd9389a0bfd20b9e9d20586538c163a92e9339
ruzempo <ruzempo@yahoo.com.mx>
2024-01-24 22:37:03 -0600

    activities week 3

---
+++ scripts/hamburger_menu.js
@@ -0,0 +1,14 @@
+//*********************************
+// For responsive hamburger menu
+// Author: Rubén Zempoalteca
+// 22.01.2024
+//*********************************
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
